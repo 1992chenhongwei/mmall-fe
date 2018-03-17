39ee2c86c25c0f229222463493a4375d93c1c299
1992chenhongwei <feng992211@163.com>
2018-03-17 18:38:00 +0800

    环境配置
    所有环境配置搭建完毕

---
+++ src/page/index/index.js
@@ -0,0 +1,6 @@
+
+require('../login/index.js');
+require('../module.js');
+require('./index.css');
+console.log("hello index");
+// alert("helloword");
---
+++ src/page/login/index.js
@@ -0,0 +1,2 @@
+require('../module.js');
+console.log("hello login----");
