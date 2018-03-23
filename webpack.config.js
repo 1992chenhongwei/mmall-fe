e5c7b8620be0ade1a6232c3ff7a6df5a21eb6a4e
1992chenhongwei <feng992211@163.com>
2018-03-23 16:40:42 +0800

    商品模块

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,8 @@ var config = {
     entry: {
         'common' : ['./src/page/common/index.js'],
         'index' : ['./src/page/index/index.js'],
+        'list' : ['./src/page/list/index.js'],
+        'detail' : ['./src/page/detail/index.js'],
         'user-login' : ['./src/page/user-login/index.js'],
         'user-register' : ['./src/page/user-register/index.js'],
         'user-pass-reset': ['./src/page/user-pass-reset/index.js'],
@@ -65,6 +67,8 @@ var config = {
         new ExtractTextPlugin("css/[name].css"),
         //html模板的处理
         new HtmlWebpackPlugin(getHtmlConfig('index','首页')),
+        new HtmlWebpackPlugin(getHtmlConfig('list','商品列表页')),
+        new HtmlWebpackPlugin(getHtmlConfig('detail','商品详情页')),
         new HtmlWebpackPlugin(getHtmlConfig('user-login','用户登录')),
         new HtmlWebpackPlugin(getHtmlConfig('user-pass-reset','找回密码')),
         new HtmlWebpackPlugin(getHtmlConfig('user-center','个人中心')),
